56c46ceb4656e54fa8ee5cd37f8f795c78c4c1c3
garedb <garedbarney@gmail.com>
2020-09-23 13:46:05 -0700

    new

--- server.js
+++ server.js
@@ -12,8 +12,7 @@ const db = knex({
   client: 'pg',
   connection: {
     connectionString : 'process.env.DATABASE_URL',
-    ssl: {
-    rejectUnauthorized: false
+    ssl: true
   }
 });
 
@@ -22,7 +21,7 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 
-app.get('/', (req, res)=> { res.send('it is working!') })
+app.get('/', (req, res)=> { res.send(db.users) })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
