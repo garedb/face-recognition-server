3eb05e7b30fa4479e56dce5297fe009f0ef4b1bb
garedb <garedbarney@gmail.com>
2020-09-23 13:41:12 -0700

    changed some server settings

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ const db = knex({
   client: 'pg',
   connection: {
     connectionString : 'process.env.DATABASE_URL',
-    ssl: true
+    ssl: {
+    rejectUnauthorized: false
   }
 });
 
@@ -21,7 +22,7 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 
-app.get('/', (req, res)=> { res.send(db.users) })
+app.get('/', (req, res)=> { res.send('it is working!') })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
